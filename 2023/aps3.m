dab1a49309645fcc90c2224a8b4b952f7964e150
Enricco Gemha <riccogemha@gmail.com>
2023-03-26 10:57:09 -0300

    continuing aps4

--- 2023/aps3.m
+++ 2023/aps3.m
@@ -1,8 +1,8 @@
 %% Ex 3a
 s = tf('s');
-G = s/(2*s+1);
+G = 50/(power(s,2)+10s+100);
 
 figure;
-step(G);        % Resposta gráfica temporal para uma entrada do tipo degrau
+step(5*G);        % Resposta gráfica temporal para uma entrada do tipo degrau
                 % de amplitude 1, i.e., y(t) por t para x(t)= 1.
 grid on
